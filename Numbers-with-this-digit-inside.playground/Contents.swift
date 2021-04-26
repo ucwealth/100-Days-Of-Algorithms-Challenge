5cdb70be3f9c398671cd5bc2f28447fd0c4c0495
ucwealth <ucheokeke16@gmail.com>
2021-04-26 21:32:29 +0100

    day8 challenge 2 completed

--- Numbers-with-this-digit-inside.playground/Contents.swift
+++ Numbers-with-this-digit-inside.playground/Contents.swift
@@ -21,12 +21,12 @@ Return: [3, 22, 110]
 If there are no numbers, which include the digit, return [0,0,0].
 */
 
-typeAlias Long = Int64
-func numbersWithDigitInside(_ x: Long, _ d: Long) -> [Long] {
+//typeAlias Long = Int64
+func numbersWithDigitInside(_ x: Int64, _ d: Int64) -> [Int64] {
 
-    var arr = [Int]()
-    var str = ""
-    var count = 0
+    var arr = [Int64]()
+   var str = ""
+    var count: Int64 = 0
     for i in 1...x {
         str = String(i)
         if str.contains(String(d)) {
@@ -34,13 +34,13 @@ func numbersWithDigitInside(_ x: Long, _ d: Long) -> [Long] {
             count += 1
         }
     }
-   let sum = arr.reduce(0, +)
-  var mult = 0
+   let sum: Int64 = arr.reduce(0, +)
+  var mult: Int64 = 0
   if arr.count > 0{
-    mult = arr.reduce(1, *)
+    mult = Int64(arr.reduce(1, *))
   }
     
-    let result = [count, sum, mult]
+    let result: [Int64] = [count, sum, mult]
     return result
 
 }
