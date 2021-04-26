7cec854adab4e923e761e1e347b2621b4c241ab9
ucwealth <ucheokeke16@gmail.com>
2021-04-26 20:28:06 +0100

    day 8 challenge 2

---
+++ Numbers-with-this-digit-inside.playground/Contents.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+/*
+You have to search all numbers from inclusive 1 to inclusive a given number x, that have the given digit d in it.
+The value of d will always be 0 - 9.
+The value of x will always be greater than 0.
+
+You have to return as an array
+
+the count of these numbers,
+their sum
+and their product.
+
+For example:
+x = 11
+d = 1
+->
+Numbers: 1, 10, 11
+Return: [3, 22, 110]
+
+If there are no numbers, which include the digit, return [0,0,0].
+*/
+
+typeAlias Long = Int64
+func numbersWithDigitInside(_ x: Long, _ d: Long) -> [Long] {
+
+    var arr = [Int]()
+    var str = ""
+    var count = 0
+    for i in 1...x {
+        str = String(i)
+        if str.contains(String(d)) {
+            arr.append(i)
+            count += 1
+        }
+    }
+   let sum = arr.reduce(0, +)
+  var mult = 0
+  if arr.count > 0{
+    mult = arr.reduce(1, *)
+  }
+    
+    let result = [count, sum, mult]
+    return result
+
+}
+
+
